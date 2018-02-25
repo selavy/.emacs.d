cd43e431b3c0d6d7269108912f2167e83e91dbaa
Peter Lesslie <pclesslie@gmail.com>
2018-02-25 10:59:13 -0600

    More gitignore

--- init.el
+++ init.el
@@ -2,17 +2,10 @@
 (setq inhibit-startup-screen 1)
 (setq inhibit-startup-message t)
 
-;; no tabs
-(setq-default indent-tabs-mode nil)
-;; indent 4 spaces, and set indent style
-(setq c-default-style "stroustrup" c-basic-offset 4)
-
-;; store all backup and autosave files in the tmp dir
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
 ;; better-defaults
 (add-to-list 'load-path "./site-lisp/better-defaults")
 (require 'better-defaults)
+
+;; indent 4 spaces, and set indent style
+(setq c-default-style "stroustrup" c-basic-offset 4)
+
