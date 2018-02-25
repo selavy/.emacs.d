45054d88769ca9d97f438cf3f994437332df5b37
Peter Lesslie <pclesslie@gmail.com>
2018-02-25 12:39:34 -0600

    Add dash

--- init.el
+++ init.el
@@ -2,9 +2,18 @@
 (setq inhibit-startup-screen 1)
 (setq inhibit-startup-message t)
 
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+(defun add-pkg-path (dir-name)
+  (add-to-list 'load-path (expand-file-name dir-name site-lisp-dir)))
+
 ;; better-defaults
-(add-to-list 'load-path "./site-lisp/better-defaults")
+(add-pkg-path "better-defaults")
 (require 'better-defaults)
+
+;; dash
+(add-pkg-path "dash")
+(require 'dash)
 
 ;; indent 4 spaces, and set indent style
 (setq c-default-style "stroustrup" c-basic-offset 4)
