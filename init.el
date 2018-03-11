711a88e0bb56d30f2ba503f813a40d9d3884a0d1
Peter Lesslie <plesslie@gmail.com>
2018-03-11 15:06:46 -0700

    Include helm-ag package

--- init.el
+++ init.el
@@ -39,6 +39,7 @@
      smex
      material-theme
      elpy
+     helm-ag
      )))
 
 (condition-case nil
@@ -60,3 +61,15 @@
 
 (global-linum-mode t)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (elpy material-theme smex magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
